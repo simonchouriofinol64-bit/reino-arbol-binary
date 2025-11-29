2b41e8a18b67f5235e3f3acef64ae339aa7fa3e4
simonchouriofinol64-bit <simonchouriofinol64@gmail.com>
2025-11-29 01:02:29 -0400

    feat:creando estructura incial de nuestro proyecto

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "sistema de sucesion de un reino version inicial" << endl;
+    cout << "este programa aun esta en construccion" << endl;
+    return 0;
+}
